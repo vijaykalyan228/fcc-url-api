84860cdd599b95d18c765d585ecad228c7e1e5d0
Glitch (fcc-url-api) <none>
2019-12-09 05:45:11 +0000

    🌆🍧 Checkpoint ./server.js:9193983/36

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.post("/api/shorturl/new")
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
